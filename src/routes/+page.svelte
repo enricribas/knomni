7a96434ea1b3d49d495e4a35d37eaf62b37c9ed7
Enric Ribas <enric.ribas@botco.ai>
2024-07-23 13:56:02 -0400

    Update messaging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,34 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-center items-center min-h-screen">
 	<div class="space-y-5">
-		<h1 class="h1">Knomni</h1>
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+				>AI.</span
+			>
+		</h1>
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
+				>Integrations.</span
+			>
+		</h1>
+		<h1 class="h1">
+			<span
+				class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
+				>Insights.</span
+			>
+		</h1>
 
-		<p>Automate Your Business</p>
-		<ul>
-			<li>AI chatbots</li>
-			<li>AI generate email campaigns</li>
-			<li>Integrations</li>
-			<li>Automations</li>
-		</ul>
+		<h1 class="h1 mt-20 text-white">Knomni.</h1>
+
+		<p class="h3 text-gray-300">AI Consultancy</p>
+		<div class="ml-10">
+			<p class="h5 text-gray-500">AI Chatbots</p>
+			<p class="h5 text-gray-500">AI customized email campaign</p>
+			<p class="h5 text-gray-500">CRM integrations</p>
+			<p class="h5 text-gray-500">Scheduled tasks</p>
+		</div>
 	</div>
 </div>
