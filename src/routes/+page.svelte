0405858dfa87747cb0f489808ee9bb4fc9cae5e7
Enric Ribas <enric.ribas@botco.ai>
2024-07-23 10:31:51 -0400

    Basic landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Knomni</h1>
+
+		<p>Automate Your Business</p>
+		<ul>
+			<li>AI chatbots</li>
+			<li>AI generate email campaigns</li>
+			<li>Integrations</li>
+			<li>Automations</li>
+		</ul>
+	</div>
+</div>
