d418edcf37be542ff65fc0d7a80bd8fc212f6de1
Enric Ribas <enric.ribas@botco.ai>
2024-07-23 14:01:54 -0400

    Improve text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center min-h-screen">
-	<div class="space-y-5">
+	<div class="">
 		<h1 class="h1">
 			<span
 				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-				>AI.</span
+				>A.I.</span
 			>
 		</h1>
 		<h1 class="h1">
@@ -21,14 +21,16 @@
 			>
 		</h1>
 
-		<h1 class="h1 mt-20 text-white">Knomni.</h1>
+		<h1 class="h1 mt-20 mb-20 text-white">Knomni.</h1>
 
-		<p class="h3 text-gray-300">AI Consultancy</p>
-		<div class="ml-10">
-			<p class="h5 text-gray-500">AI Chatbots</p>
-			<p class="h5 text-gray-500">AI customized email campaign</p>
-			<p class="h5 text-gray-500">CRM integrations</p>
-			<p class="h5 text-gray-500">Scheduled tasks</p>
+		<div class="h-10 space-y-10">
+			<p class="h3 text-gray-300">AI Consultancy</p>
+			<div class="ml-10">
+				<p class="h5 text-gray-500">A.I. chatbots</p>
+				<p class="h5 text-gray-500">A.I. customized email campaigns</p>
+				<p class="h5 text-gray-500">CRM integrations</p>
+				<p class="h5 text-gray-500">Scheduled tasks</p>
+			</div>
 		</div>
 	</div>
 </div>
