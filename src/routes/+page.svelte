2491b23d4d74d01032c8741443e5a9cbaf020a2e
Enric Ribas <enric.ribas@botco.ai>
2024-07-24 08:16:36 -0400

    Change fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center min-h-screen">
-	<div class="">
+	<div>
 		<h1 class="h1">
 			<span
 				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
@@ -24,12 +24,13 @@
 		<h1 class="h1 mt-20 mb-20 text-white">Knomni.</h1>
 
 		<div class="h-10 space-y-10">
-			<p class="h3 text-gray-300">AI Consultancy</p>
+			<p class="h3 text-gray-300 font-sans">AI Consultancy</p>
 			<div class="ml-10">
-				<p class="h5 text-gray-500">A.I. chatbots</p>
-				<p class="h5 text-gray-500">A.I. customized email campaigns</p>
-				<p class="h5 text-gray-500">CRM integrations</p>
-				<p class="h5 text-gray-500">Scheduled tasks</p>
+				<p class="h5 text-gray-500 font-sans">A.I. chatbots</p>
+				<p class="h5 text-gray-500 font-sans">A.I. customized email campaigns</p>
+				<p class="h5 text-gray-500 font-sans">CRM integrations</p>
+				<p class="h5 text-gray-500 font-sans">CRM data cleansing</p>
+				<p class="h5 text-gray-500 font-sans">Scheduled tasks</p>
 			</div>
 		</div>
 	</div>
