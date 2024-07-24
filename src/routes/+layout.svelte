99f49f6da68ca7766ad7dec5f55369f061d3ad2c
Enric Ribas <enric.ribas@botco.ai>
2024-07-24 15:29:24 -0400

    Add bad logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.postcss';
 </script>
 
+<img src="/logo.png" alt="Logo" style="max-width: 150px" />
 <slot />
+
+<style>
+	img {
+		position: absolute;
+		top: 20px;
+		left: 20px;
+	}
+</style>
